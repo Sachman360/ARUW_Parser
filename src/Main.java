b920112c9bdcccb6a0455f298a69bdaeb64012f7
sachal <sachalpshaikh@gmail.com>
2023-09-29 22:49:02 -0700

    Removed most comments :)

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // into the Parser.
         int[] data = {2, 1, 2, 1, 1, 2, 3, 1, 4, 5, 2, 2, 1, 4, 5, 9, 1, 7, 8, 2, 3, 4, 5, 6, 7, 8, 9, 1};
         feedData(p, data);
+
         System.out.println(p.dataToString());
         System.out.println(p);
     }
--- src/Parser.java
+++ src/Parser.java
@@ -1,27 +1,22 @@
+// Sachal Shaikh
+// ARUW Software Technical Question
+
 import java.util.*;
 
 public class Parser {
     /** Declaration of all class variables **/
 
-    // data stores all words inputted to the Parser. It is not strictly
-    // necessary for the rules of this project, but rather a helpful addition.
+    // Note: "data" is not necessary for the parser to run properly.
     ArrayList<Integer> data;
-    // packets is a list of lists. Each inner list is a packet.
     ArrayList<ArrayList<Integer>> packets;
-    // The following two boolean variables operate in the same way. isVelocity
-    // is set to true if an incoming word (at the beginning of a packet) is a 1,
-    // and set to false when a packet ends. Same for isVision (with an incoming 2).
     boolean isVelocity;
     boolean isVision;
-    // numValues keeps track of the number of values in a packet, and is checked
-    // against a max value that changes based on the particular packet, to see
-    // when the packet should end.
     int numValues;
     int max;
 
     public Parser() {
-        data = new ArrayList<Integer>();
-        packets = new ArrayList<ArrayList<Integer>>();
+        data = new ArrayList<>();
+        packets = new ArrayList<>();
         isVelocity = false;
         isVision = false;
         numValues = 0;
@@ -34,19 +29,14 @@ public class Parser {
 
     /** addWord is the main method used in this program. It receives and interprets
      * data in the form of int words and puts them into packets. **/
+
     public void addWord(int n) {
         data.add(n);
         numValues++;
         // If a word is the first word in a packet, the following code is run:
         if(numValues == 1) {
-            // A new list is added to packets (representing a new packet).
             packets.add(new ArrayList<Integer>());
-            // The word n is added to this new list (even though this action occurs
-            // in both parts of the if-else, it must be completed in here since the
-            // list is created in here).
             packets.get(packets.size() - 1).add(n);
-            // Determines which type of packet it is, and sets the corresponding
-            // boolean to true.
             if(n == 1) {
                 isVelocity = true;
                 // For a wheelVelocity packet, we know the packet size of 3 right away.
@@ -58,7 +48,6 @@ public class Parser {
             }
         // If a word is not the first in a packet, the following code is run:
         } else {
-            // New word is added to the latest packet.
             packets.get(packets.size() - 1).add(n);
             // If the packet is a visionTarget packet, the second word must be used to
             // determine the packet size, represented by the variable max.
